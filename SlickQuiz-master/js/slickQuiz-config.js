7f31f384e3dc374fb7fa5a5e57df7cae1249fe50
averyhw <averyhwilliams@yahoo.com>
2017-10-10 19:04:30 -0400

    quiz added

---
+++ SlickQuiz-master/js/slickQuiz-config.js
@@ -0,0 +1,74 @@
+// Setup your quiz text and questions here
+
+// NOTE: pay attention to commas, IE struggles with those bad boys
+
+var quizJSON = {
+    "info": {
+        "name":    "Test Your Knowledge!",
+        "main":    "<p class='quiz-space'>Think you know all about recycling now? Take this quiz and find out</p>",
+        "results": "<h5>Learn more at the EPA's website! Happy recycling!</h5>",
+        "level1":  "Recycle Pro",
+        "level2":  "Intermediate Recycler ",
+        "level3":  "Not a Good Recycler",
+        "level4":  "Never Recycled",
+        "level5":  "What is recycling?" // no comma here
+    },
+    "questions": [
+        { // Question 1 - Multiple Choice, Single True Answer
+            "q": "<p class='quiz-space'>How many millions of tons of trash are created every year in the U.S. alone?</p>",
+            "a": [
+                {"option": "258",      "correct": true},
+                {"option": "534",     "correct": false},
+                {"option": "285",      "correct": false},
+                {"option": "679",     "correct": false} // no comma here
+            ],
+            "correct": "<p class='quiz-space'><span>Correct!</span> The U.S. produces 258 million tons of trash each year...talk about a dump.</p>",
+            "incorrect": "<p class='quiz-space'><span>Whoops!</span> Yes, the first question is supposed to be easy, but this is just too important. The U.S. creates 258 million tons of trash each year.</p>" // no comma here
+        },
+        { // Question 2 - Multiple Choice, Multiple True Answers, Select Any
+            "q": "<p class='quiz-space'>Which of the following is not a way for recycled material to be collected?</p>",
+            "a": [
+                {"option": "curb-side",               "correct": false},
+                {"option": "drop-off",   "correct": false},
+                {"option": "deposit",               "correct": false},
+                {"option": "throwing it out the window of your car", "correct": true} // no comma here
+            ],
+            "select_any": true,
+            "correct": "<p class='quiz-space'><span>Phew!</span> Thank you for recycling and not littering.</p>",
+            "incorrect": "<p class='quiz-space'><span>Yikes!</span> You can leave it curb-side, drop it off and leave it at a deposit — you don't have to throw it out the window!</p>" // no comma here
+        },
+        { // Question 3 - Multiple Choice, Multiple True Answers, Select All
+            "q": "<p class='quiz-space'>Which item is made from recycled materials? Select ALL that apply.</p>",
+            "a": [
+                {"option": "newspaper",           "correct": true},
+                {"option": "a couch",                  "correct": false},
+                {"option": "steel can",  "correct": true},
+                {"option": "road",          "correct": true} // no comma here
+            ],
+            "correct": "<p class='quiz-space'><span>Awesome!</span> Looks like someone read the slides!</p>",
+            "incorrect": "<p class='quiz-space'><span>So close...</span> Maybe your couch is made from recycled materials, but chances are slim.</p>" // no comma here
+        },
+        { // Question 4
+            "q": "<p class='quiz-space'>What type of plastic is something if it has a number 1 recycle symbol?</p>",
+            "a": [
+                {"option": "PET",    "correct": true},
+                {"option": "XYZ",     "correct": false},
+                {"option": "PPP",      "correct": false},
+                {"option": "ABC",   "correct": false} // no comma here
+            ],
+            "correct": "<p class='quiz-space'><span>Recycle pro over here!</span> An example of a number 1 plastic is a peanut butter jar (but you probably already know that).</p>",
+            "incorrect": "<p class='quiz-space'><span>Whoops!</span> Those aren't even real — go back to the slides.</p>" // no comma here
+        },
+        { // Question 5
+            "q": "<p class='quiz-space'>How can you continue to recycle on your own? Select ALL that apply.</p>",
+            "a": [
+              {"option": "buy recycled when you can",    "correct": true},
+              {"option": "use energy efficient bulbs",     "correct": true},
+              {"option": "compost leftovers",      "correct": true},
+              {"option": "visit the EPA's website on recycling in my area",   "correct": true} // no comma here
+            ],
+            "correct": "<p class='quiz-space'><span>Yay!</span> These are all great ways to recycle on your own.</p>",
+            "incorrect": "<p class='quiz-space'><span>Almost there...</span> These are <em>all</em> great ways to recycle on your own!</p>" // no comma here
+        } // no comma here
+    ]
+};
