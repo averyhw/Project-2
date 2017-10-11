0cead5b0d454d4eda3c34a5d0adfbeee279715d3
averyhw <averyhwilliams@yahoo.com>
2017-10-11 02:40:44 -0400

    almost done

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,23 @@
 //READY FUNCTION
 $(document).ready(function() {
 	$('#pagepiling').pagepiling();
-	$('.tooltip').tooltipster();
+	$('.title-tipso').tipso({
+		speed       : 400,
+    background  : '#55b555',
+    color       : '#ffffff',
+    position    : 'top',
+    width       : 200,
+    delay       : 200,
+    offsetX     : 0,
+    offsetY     : 0,
+    content     : null,
+    useTitle    : true,
+    onShow      : null,
+    onHide      : null
+	});
 });
+
+
 
 
 $(function () {
